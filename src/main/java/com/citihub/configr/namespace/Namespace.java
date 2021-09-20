36f498e5bcc110af40a9a7f9a064ad2fd3ce1e30
Adam Schwartz <adam.schwartz@citihub.com>
2021-09-20 15:58:30 -0400

    Auto-creates index, reworks Mongo scaffolding for tests

--- src/main/java/com/citihub/configr/namespace/Namespace.java
+++ src/main/java/com/citihub/configr/namespace/Namespace.java
@@ -6,16 +6,12 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import com.citihub.configr.version.Version;
 import com.fasterxml.jackson.annotation.JsonKey;
 import com.fasterxml.jackson.annotation.JsonValue;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 @Data
 @Slf4j
 @Document
-@JsonSerialize(using = NamespaceSerializer.class)
-@JsonDeserialize(using = NamespaceDeserializer.class)
 public class Namespace {
 
   private Metadata metadata;
@@ -45,5 +41,5 @@ public class Namespace {
     this.value = value;
     this.namespace = fullNamespace;
   }
-
+  
 }
