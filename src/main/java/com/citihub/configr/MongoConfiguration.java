a7025733296fa874b84b70016d62a449157670d6
Adam Schwartz <adam.schwartz@citihub.com>
2021-10-27 16:00:12 -0400

    Adds x509 auth support for MongoDB

--- src/main/java/com/citihub/configr/MongoConfiguration.java
+++ src/main/java/com/citihub/configr/MongoConfiguration.java
@@ -1,13 +1,17 @@
 package com.citihub.configr;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.MongoClientFactoryBean;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import com.google.common.base.Strings;
 import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoCredential;
+import com.mongodb.client.MongoClient;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -21,6 +25,12 @@ public class MongoConfiguration {
   @Value("${mongodb.uri}")
   private String uri;
 
+  @Value("${mongodb.mongo_keystore_path}")
+  private String keyStorePath;
+
+  @Value("${mongodb.mongo_keystore_password}")
+  private String keyStorePassword;
+
   @Value("${mongodb.username}")
   private String username;
 
@@ -33,10 +43,27 @@ public class MongoConfiguration {
   public @Bean MongoClientFactoryBean mongo() {
     MongoClientFactoryBean mongo = new MongoClientFactoryBean();
     mongo.setConnectionString(new ConnectionString(uri));
-    if (!Strings.isNullOrEmpty(username))
-      mongo.setCredential(new MongoCredential[] {
-          MongoCredential.createCredential(username, db, password.toCharArray())});
+    if (!Strings.isNullOrEmpty(keyStorePath))
+      setX509Auth(mongo);
+    else if (!Strings.isNullOrEmpty(username))
+      setBasicCredentialAuth(mongo);
+
     return mongo;
+  }
+
+  public @Bean MongoTemplate mongoTemplate(@Autowired MongoClient mongo) {
+    return new MongoTemplate(mongo, db);
+  }
+
+  private void setBasicCredentialAuth(MongoClientFactoryBean mongo) {
+    mongo.setCredential(new MongoCredential[] {
+        MongoCredential.createCredential(username, db, password.toCharArray())});
+  }
+
+  private void setX509Auth(MongoClientFactoryBean mongo) {
+    mongo.setMongoClientSettings(
+        MongoClientSettings.builder().credential(MongoCredential.createMongoX509Credential())
+            .applyToSslSettings(b -> b.enabled(true)).build());
   }
 
 }
