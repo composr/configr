f80120b8f23631f392638c0c9e3ea383abd03749
Adam Schwartz <adam.schwartz@citihub.com>
2021-10-29 13:36:44 -0400

    Removes flapdoodle, adds in-memory mongo for testing

---
+++ src/test/java/com/citihub/configr/TestMongoConfiguration.java
@@ -0,0 +1,36 @@
+package com.citihub.configr;
+
+import java.net.InetSocketAddress;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+import de.bwaldvogel.mongo.MongoServer;
+import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
+
+@Configuration
+@Profile("test")
+public class TestMongoConfiguration {
+
+  @Bean
+  public MongoTemplate mongoTemplate(MongoDatabaseFactory mongoDatabaseFactory) {
+    return new MongoTemplate(mongoDatabaseFactory);
+  }
+
+  @Bean
+  public MongoDatabaseFactory mongoDbFactory(MongoServer mongoServer) {
+    InetSocketAddress serverAddress = mongoServer.getLocalAddress();
+    return new SimpleMongoClientDatabaseFactory(
+        "mongodb://" + serverAddress.getHostName() + ":" + serverAddress.getPort() + "/test");
+  }
+
+  @Bean(destroyMethod = "shutdown")
+  public MongoServer mongoServer() {
+    MongoServer mongoServer = new MongoServer(new MemoryBackend());
+    mongoServer.bind();
+    return mongoServer;
+  }
+
+}
