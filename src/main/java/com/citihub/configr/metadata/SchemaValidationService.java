66c0c40f78297141f2e53febf73e19ca0d15dbd0
GGG <luis.carrazana@citihub.com>
2022-01-18 16:55:33 -0500

    Refactor: removing unused vars. Removing snake_case from test names. Removing comments from tests and pdating method names.

--- src/main/java/com/citihub/configr/metadata/SchemaValidationService.java
+++ src/main/java/com/citihub/configr/metadata/SchemaValidationService.java
@@ -1,10 +1,8 @@
 package com.citihub.configr.metadata;
 
 import java.io.IOException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.citihub.configr.exception.SchemaValidationException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,37 +13,35 @@ import com.github.fge.jsonschema.main.JsonSchemaFactory;
 
 @Service
 public class SchemaValidationService {
-	
-	private ObjectMapper objectMapper;
-	
-	public SchemaValidationService(@Autowired ObjectMapper objectMapper) {
-		this.objectMapper = objectMapper;
-	}
-	
-	public SchemaValidationResult validateJSON(final String json, final String schema)
-		throws SchemaValidationException
-	{
-	  
-	  if (json == null || schema == null)
-		  throw new SchemaValidationException("Inputs cannot be null");
-	  
-	  try {
-		final JsonNode jsonObj = this.objectMapper.readTree(json);
-		final JsonNode schemaObj = this.objectMapper.readTree(schema);
-		
-		final JsonSchema jsonSchema = JsonSchemaFactory.byDefault().getJsonSchema(schemaObj);
-		
-		ProcessingReport report = jsonSchema.validate(jsonObj);
-		
-		SchemaValidationResult result = new SchemaValidationResult(report.isSuccess(), report.toString());
-		
-		return result;
-		
-	  } catch (IOException | ProcessingException e) {
-		e.printStackTrace();
-		throw new SchemaValidationException("An error occurred while processing schema validation. Check logs.");
-	  }
-	  
-	}
+
+  private ObjectMapper objectMapper;
+
+  public SchemaValidationService(@Autowired ObjectMapper objectMapper) {
+    this.objectMapper = objectMapper;
+  }
+
+  public SchemaValidationResult validateJSON(final String json, final String schema)
+      throws SchemaValidationException {
+
+    if (json == null || schema == null)
+      throw new SchemaValidationException("Inputs cannot be null");
+
+    try {
+      final JsonNode jsonObj = this.objectMapper.readTree(json);
+      final JsonNode schemaObj = this.objectMapper.readTree(schema);
+
+      final JsonSchema jsonSchema = JsonSchemaFactory.byDefault().getJsonSchema(schemaObj);
+
+      ProcessingReport report = jsonSchema.validate(jsonObj);
+
+      return new SchemaValidationResult(report.isSuccess(), report.toString());
+
+    } catch (IOException | ProcessingException e) {
+      e.printStackTrace();
+      throw new SchemaValidationException(
+          "An error occurred while processing schema validation. Check logs.");
+    }
+
+  }
 
 }
--- src/test/java/com/citihub/configr/metadata/SchemaValidationServiceTest.java
+++ src/test/java/com/citihub/configr/metadata/SchemaValidationServiceTest.java
@@ -1,13 +1,11 @@
 package com.citihub.configr.metadata;
 
-import com.citihub.configr.base.UnitTest;
-import com.citihub.configr.exception.SchemaValidationException;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import com.citihub.configr.base.UnitTest;
+import com.citihub.configr.exception.SchemaValidationException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class SchemaValidationServiceTest extends UnitTest {
@@ -54,73 +52,80 @@ public class SchemaValidationServiceTest extends UnitTest {
   }
 
   @Test
-  public void testValidateJSON_WithInvalid_ShallFail() {
+  public void testValidateJSONWithInvalidMinimumShallFail() {
 
-    String jsonInvalidPriceValue = """
+    String jsonInvalidMinimumPriceValue = """
         {
         	"id": 1,
         	"name": "book",
         	"price": 0
         }
         """;
-    // According to schema, "price" shall be exclusively greater than 0, so this should fail
-    var result = validationService.validateJSON(jsonInvalidPriceValue, jsonSchema);
+    var result = validationService.validateJSON(jsonInvalidMinimumPriceValue, jsonSchema);
     assertThat(result.getIsSuccess()).isFalse();
+
+  }
+
+  @Test
+  public void testValidateJSONWithInvalidTypeShallFail() {
 
     String jsonInvalidIDType = """
         {
-        	"id": "not a number",
-        	"name": "book",
-        	"price": 0
+            "id": "not a number",
+            "name": "book",
+            "price": 0
         }
         """;
-    // According to schema, "id" shall be of type integer, so this should fail
-    result = validationService.validateJSON(jsonInvalidIDType, jsonSchema);
+    var result = validationService.validateJSON(jsonInvalidIDType, jsonSchema);
     assertThat(result.getIsSuccess()).isFalse();
+
+  }
+
+  @Test
+  public void testValidateJSONWithMissingRequiredFieldShallFail() {
 
     String jsonInvalidMissingField = """
         {
-        	"id": 1,
-        	"price": 2
+            "id": 1,
+            "price": 2
         }
         """;
-    // According to schema, "name" is required, so this should fail
-    result = validationService.validateJSON(jsonInvalidMissingField, jsonSchema);
+    var result = validationService.validateJSON(jsonInvalidMissingField, jsonSchema);
     assertThat(result.getIsSuccess()).isFalse();
 
   }
 
   @Test
-  public void testValidateJSON_WithValid_ShallSucceed() {
+  public void testValidateJSONWithValidShallSucceed() {
 
-    // According to schema:
-    // "id" shall be of type integer
-    // "name" shall be of type string
-    // "price" shall be exclusively greater than 0
     var result = validationService.validateJSON(jsonValid, jsonSchema);
     assertThat(result.getIsSuccess()).isTrue();
+
   }
 
   @Test
-  public void testValidateJSON_WithNullOrMalformed_ShallRaiseException() {
+  public void testValidateJSONWithNullShallRaiseException() {
 
-    String jsonInvalidSyntax = """
-        {
-        	"id": 1,
-        	"name": "book
-        	"price": 2
-        }
-        """;
-    // If json object is malformed this should fail
-    assertThrows(SchemaValidationException.class,
-        () -> validationService.validateJSON(jsonInvalidSyntax, jsonSchema));
-
-    // If json and/or schema inputs are null this should fail
     assertThrows(SchemaValidationException.class, () -> validationService.validateJSON(null, null));
     assertThrows(SchemaValidationException.class,
         () -> validationService.validateJSON(null, jsonSchema));
     assertThrows(SchemaValidationException.class,
         () -> validationService.validateJSON(jsonValid, null));
+  }
+
+  @Test
+  public void testValidateJSONWithMalformedShallRaiseException() {
+
+    String jsonInvalidSyntax = """
+        {
+            "id": 1,
+            "name": "book
+            "price": 2
+        }
+        """;
+    assertThrows(SchemaValidationException.class,
+        () -> validationService.validateJSON(jsonInvalidSyntax, jsonSchema));
+
   }
 
 }
